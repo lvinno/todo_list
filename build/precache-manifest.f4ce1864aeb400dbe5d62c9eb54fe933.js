40cf1628db5e5506ac82a3104577075196e0dbd3
lvino <lvino@lvinodeMacBook-Pro.local>
2019-01-07 14:10:09 +0800

    build commit

---
+++ build/precache-manifest.f4ce1864aeb400dbe5d62c9eb54fe933.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "e5089bea54074d54ba48",
+    "url": "./static/js/main.e5089bea.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "./static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "e5089bea54074d54ba48",
+    "url": "./static/css/main.62760b40.chunk.css"
+  },
+  {
+    "revision": "2d4d3140268a964b514fd51ddad0aaf8",
+    "url": "./index.html"
+  }
+];
