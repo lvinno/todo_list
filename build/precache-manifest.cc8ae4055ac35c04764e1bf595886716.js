d31450aff4180a1d072a72880215beee2beda1cb
lvino <553269233@qq.com>
2019-01-15 20:31:22 +1100

    update

--- build/precache-manifest.cc8ae4055ac35c04764e1bf595886716.js
+++ build/precache-manifest.cc8ae4055ac35c04764e1bf595886716.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "./static/js/runtime~main.4a686d48.js"
   },
   {
-    "revision": "ef4b14c3d4373249c694",
-    "url": "./static/js/main.ef4b14c3.chunk.js"
+    "revision": "61f1141bbc1b40336350",
+    "url": "./static/js/main.61f1141b.chunk.js"
   },
   {
     "revision": "38a5d9b4ba97eeb1dec7",
     "url": "./static/js/1.38a5d9b4.chunk.js"
   },
   {
-    "revision": "ef4b14c3d4373249c694",
+    "revision": "61f1141bbc1b40336350",
     "url": "./static/css/main.dc0a038d.chunk.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "./static/css/1.48a63d83.chunk.css"
   },
   {
-    "revision": "60cb7f4095320dd9c5f5a1b24872b076",
+    "revision": "f9cfeee9e802526f230a40c8897150ed",
     "url": "./index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5867784f6056dca1f1f74a2f8687b979.js"
+  "./precache-manifest.cc8ae4055ac35c04764e1bf595886716.js"
 );
 
 workbox.clientsClaim();
